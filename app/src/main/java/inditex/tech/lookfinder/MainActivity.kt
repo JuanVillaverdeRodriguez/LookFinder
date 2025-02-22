b3021d2a08640cd9ab7ca87fec331c9dfcd8ae41
JackCubas <alexcubas.1508@gmail.com>
2025-02-22 01:53:30 +0100

    Primera iteración viewModel

--- app/src/main/java/inditex/tech/lookfinder/MainActivity.kt
+++ app/src/main/java/inditex/tech/lookfinder/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,19 +12,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import inditex.tech.lookfinder.api.getApiResponse
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import inditex.tech.lookfinder.ui.theme.LookFinderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        lifecycleScope.launch {
+
+        }
         setContent {
             LookFinderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Row {
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
